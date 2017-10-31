c42153052a4f6eba47af86146832ec7123e612c6
Fabiano Cunha <cunhasb@aol.com>
2017-10-31 11:52:47 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  attr_accessor :name
+
+  @@all =[]
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all=[]
+  end
+
+  def self.all
+    @@all.each{|x|puts x.name}
+  end
+
+end
